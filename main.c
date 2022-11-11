87a34feee221eda55f2180856d85bc88279167b7
Siddhesh Bhujbal <64003654+SiddheshBhujbal@users.noreply.github.com>
2022-11-11 16:45:59 +0530

    main.c pushed to remote

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {    
+
+    int num1, num2, sum;
+    printf("Enter two integers: ");
+    scanf("%d %d", &numb1, &numb2);
+    sum = numb1 + numb2;      
+    printf("%d + %d = %d", num1, numb2, sum);
+    return 0;
+}
+
